5c736b2f2a7e60a108247d2b4d72d074490ac589
Gustav Persson <gustav@alltomminrov.se>
2014-03-04 20:34:15 +0100

    Förstörde lite väl mycket, börjar om

--- Bejeweled/main.cpp
+++ Bejeweled/main.cpp
@@ -1,3 +1,7 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2013)
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 
@@ -21,7 +25,7 @@ int main(int argc, char* args[])
 	else
 	{
 		//Create window
-		window = SDL_CreateWindow("Bejeweled", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (window == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
